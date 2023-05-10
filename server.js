9abde9bd50c39acb3bc98d73e8530a8fa96b1b42
Cassidy Boilley <cboilley@my.bcit.ca>
2023-05-10 08:53:01 -0700

    added server.js, procfile, index.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const app = require('./index.js');
+
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  console.log("connected to db");
+  app.listen(3040, () => {
+    console.log('server is running on port 3020');
+  });
+}
