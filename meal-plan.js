e2ae0ddbb989ef7275a4b64a355d2900d2b8f6ce
Joshua Chuah <joshua2004cc@gmail.com>
2023-05-11 20:43:14 -0700

    add functionality to save meals to meal plan

--- meal-plan.js
+++ meal-plan.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const fastfoodCollection = require('./models/fastfoodCollection.js');
 const mealplanCollection = require('./models/mealplanCollection.js');
@@ -7,6 +8,8 @@ const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     // CHANGE AUTHENTICATED CONDITION WHEN IMPLEMENTATION IS DONE
@@ -23,6 +26,39 @@ app.get('/meal-plan', async (req, res) => {
     const result = await mealplanCollection.find();
     console.log(result);
     res.render('meal-plan.ejs', { authenticated: true, mealplan: result });
+});
+
+app.post('/save', async (req, res) => {
+    const mealId = req.body;
+    const result = await fastfoodCollection.findOne({
+        _id: mealId.mealId
+    });
+
+    try {
+        const addToMealPlan = new mealplanCollection({
+        restaurant: result.restaurant,
+        item: result.item,
+        calories: result.calories,
+        cal_fat: result.cal_fat,
+        total_fat: result.total_fat,
+        sat_fat: result.sat_fat,
+        trans_fat: result.trans_fat,
+        cholesterol: result.cholesterol,
+        sodium: result.sodium,
+        total_carb: result.total_carb,
+        fiber: result.fiber,
+        sugar: result.sugar,
+        protein: result.protein,
+        vit_a: result.vit_a,
+        vit_c:  result.vit_c,
+        calcium: result.calcium,
+        salad: result.salad
+    });
+    await addToMealPlan.save();
+    res.redirect('/fast-food');
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = app;
