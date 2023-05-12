aeacd81df0081573d06cfbc67a304c6c54b05de8
Joshua Chuah <joshua2004cc@gmail.com>
2023-05-11 18:10:04 -0700

    set up database connection

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = require('./index.js');
+const dotenv = require('dotenv');
+require('dotenv').config();
 
 main().catch(err => console.log(err));
 
 async function main() {
+
+  await mongoose.connect(`mongodb+srv://${process.env.MDBUSERNAME}:${process.env.MDBPASSWORD}@dtc14.e61wbsk.mongodb.net/?retryWrites=true&w=majority`);
+
   console.log("connected to db");
-  app.listen(3040, () => {
+  app.listen(process.env.PORT || 3040, () => {
     console.log('server is running on port 3020');
   });
 }
