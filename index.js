04b59237ba9eabb070862f10911667ec94ca6e37
Cassidy Boilley <cboilley@my.bcit.ca>
2023-05-11 18:13:50 -0700

    feature support

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
+const session = require('express-session');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+var MongoDBStore = require('connect-mongodb-session')(session);
+const dotenv = require('dotenv');
+const e = require('express');
+dotenv.config();
+
+var dbStore = new MongoDBStore({
+  uri: process.env.MDB_CONNECTION_STRING,
+  collection: 'test'
+});
 
 app.get('/', (req, res) => {
     // CHANGE AUTHENTICATED CONDITION WHEN IMPLEMENTATION IS DONE
     res.render('index.ejs', { authenticated: true });
     }
 );
+
+app.get('/recommendations', async(req, res) => {
+    res.render('recommendations.ejs', { authenticated: true });
+    
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
 const mongoose = require('mongoose');
 const app = require('./index.js');
+const dotenv = require('dotenv')
+dotenv.config();
 
 main().catch(err => console.log(err));
 
 async function main() {
+  await mongoose.connect(`${process.env.MDBCONNECTION_STRING}`);
   console.log("connected to db");
   app.listen(3040, () => {
-    console.log('server is running on port 3020');
+    console.log('server is running on port 3040');
   });
 }
