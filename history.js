3b637f09253b27928266f84b1c1083ad28bdc8fa
Wilbster <95505563+Wilbster@users.noreply.github.com>
2023-05-17 16:13:34 -0700

    Implement Description of History

--- history.js
+++ history.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const historyItemModel = require('./models/historyItem');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -10,8 +19,23 @@ app.use(express.static('public'));
 
 app.get('/history', async (req, res) => {
     const result = await historyItemModel.find({});
+
+    
     // TODO: Temporary authentication code, update later
-    res.render('history.ejs', {historyItemList: result, authenticated: true});
+    const startPrompt = "Write a paragraph of less than 120 words which greets a user named Bob, tells him that" +
+        "the following list is his history of meals, and summarizes the list"
+
+    async function runCompletion() {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: startPrompt + result,
+            max_tokens: 150
+        });
+        console.log(completion.data.choices[0].text)
+        res.render('history.ejs', { authenticated: true, historyItemList: result, message: completion.data.choices[0].text });
+    }
+    runCompletion();
+    // res.render('history.ejs', {historyItemList: result, authenticated: true});
 });
 
 module.exports = app;
