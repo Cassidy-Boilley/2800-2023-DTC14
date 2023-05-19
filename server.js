d15cb8952338d89c440efb6cc68002e80d4144a6
Cassidy Boilley <113379550+Cassidy-Boilley@users.noreply.github.com>
2023-05-19 00:25:35 -0700

    Revert "Recommendation generator cb"

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
-
 // Main page
 const app = require('./index.js');
-
 
 // dotenv setup
 const dotenv = require('dotenv');
@@ -10,12 +8,10 @@ dotenv.config();
 
 // Other pages
 const mealPlan = require('./meal-plan.js');
-const recommendations = require('./recommendations.js');
 app.use('/', mealPlan);
 main().catch(err => console.log(err));
 
 async function main() {
-
   await mongoose.connect(process.env.MDBCONNECTION_STRING);
   console.log("connected to db");
   app.listen(3040, () => {
