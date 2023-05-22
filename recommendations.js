1b74235261262814d554e356d96c68d9ce63eb53
Wilbster <95505563+Wilbster@users.noreply.github.com>
2023-05-22 08:39:59 -0700

    Update recommendations.js

--- recommendations.js
+++ recommendations.js
@@ -14,7 +14,7 @@ const mealplanCollection = require('./models/mealplanCollection.js');
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,  
 });
 const openai = new OpenAIApi(configuration);
 
